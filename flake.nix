961eb1522a713316153ffdf7f480ab75862433c1
chrisaddy <chris.william.addy@gmail.com>
2025-05-28 10:12:58 -0400

    init

--- flake.nix
+++ flake.nix
@@ -1,15 +1,69 @@
 {
-  description = "A very basic flake";
+  description = "development shell for nural";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = inputs: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+              inputs.self.overlays.default
+            ];
+          };
+        });
+  in {
+    overlays.default = final: prev: {
+      rustToolchain = let
+        rust = prev.rust-bin;
+      in
+        if builtins.pathExists ./rust-toolchain.toml
+        then rust.fromRustupToolchainFile ./rust-toolchain.toml
+        else if builtins.pathExists ./rust-toolchain
+        then rust.fromRustupToolchainFile ./rust-toolchain
+        else
+          rust.stable.latest.default.override {
+            extensions = ["rust-src" "rustfmt"];
+          };
+    };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShellNoCC {
+        name = "nural-dev-shell";
+        shell = "${pkgs.nushell}/bin/nu";
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+        # Optional: make nushell available in PATH too
+        buildInputs = [pkgs.nushell];
 
+        packages = with pkgs; [
+          rustToolchain
+          openssl
+          pkg-config
+          cargo-deny
+          cargo-edit
+          cargo-watch
+          rust-analyzer
+          mask
+        ];
+
+        env = {
+          RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+        };
+
+        shellHook = ''
+          exec ${pkgs.nushell}/bin/nu
+        '';
+      };
+    });
   };
 }
